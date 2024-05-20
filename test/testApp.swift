a0a3e3dd446fa2a425d58554bd6f36779f86e2fe
Jonathan Aaron Wibawa <aaron.wibawa@gmail.com>
2024-05-20 16:06:58 +0700

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Jonathan Aaron Wibawa on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
